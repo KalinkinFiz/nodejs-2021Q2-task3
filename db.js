923cf37dab8e11a5e64583f02a71465977f9e7fc
Maxim <kalinkin.fiz@gmail.com>
2021-05-18 18:49:12 +0300

    Initial commit

---
+++ db.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+                                //database username   password
+const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
+    host: 'localhost',
+    dialect: 'postgres'
+})
+
+sequelize.authenticate().then(
+    function success() {
+        console.log("Connected to DB");
+    },
+
+    function fail(err) {
+        console.log(`Error: ${err}`);
+    }
+)
