883eead01adee89ef1583c903f50ad6abdda6c8d
Maxim Kalinin <kalinkin.fiz@gmail.com>
2021-05-22 02:14:49 +0300

    fix: specified port number on localhost

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(express.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(4000, function() {
     console.log("App is listening on 4000");
 })
