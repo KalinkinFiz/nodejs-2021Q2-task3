8d47695f92d040d442e96285d0c766bc6376a125
Maxim Kalinin <kalinkin.fiz@gmail.com>
2021-05-22 00:36:06 +0300

    fix: user model is not imported correctly

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 
-var User = require('../db').import('../models/user');
+var User = require('../models/user');
 
 router.post('/signup', (req, res) => {
     User.create({
