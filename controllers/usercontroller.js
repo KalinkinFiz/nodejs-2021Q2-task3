2ba7903c8d247876506118767d3e0eed0ccd42d0
Maxim Kalinin <kalinkin.fiz@gmail.com>
2021-05-23 19:08:44 +0300

    fix: username verification

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -5,26 +5,33 @@ var jwt = require('jsonwebtoken');
 var User = require('../db').import('../models/user');
 
 router.post('/signup', (req, res) => {
-    User.create({
-        full_name: req.body.user.full_name,
-        username: req.body.user.username,
-        passwordHash: bcrypt.hashSync(req.body.user.password, 8),
-        email: req.body.user.email,
-    })
-        .then(
-            function signupSuccess(user) {
-                let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
-                res.status(200).json({
-                    user: user,
-                    token: token
-                })
-            },
+    User.findOne({ where: { username: req.body.user.username } }).then(user => {
+        if (user) {
+            res.status(400).send({ error: "User whith this name exists." });
+        } else {
+            User.create({
+                full_name: req.body.user.full_name,
+                username: req.body.user.username,
+                passwordHash: bcrypt.hashSync(req.body.user.password, 10),
+                email: req.body.user.email,
+            })
+                .then(
+                    function signupSuccess(user) {
+                        let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+                        res.status(200).json({
+                            user: user,
+                            token: token
+                        })
+                    },
 
-            function signupFail(err) {
-                res.status(500).send(err.message)
-            }
-        )
+                    function signupFail(err) {
+                        res.status(500).send(err.message)
+                    }
+                )
+        }
+    });   
 })
+    
 
 router.post('/signin', (req, res) => {
     User.findOne({ where: { username: req.body.user.username } }).then(user => {
